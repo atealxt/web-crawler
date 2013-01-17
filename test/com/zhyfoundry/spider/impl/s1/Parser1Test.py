d35d938241f10ff680949d686ffb3ca66e7b6ffe
Luis Ashurei <atealxt@gmail.com>
2013-01-16 23:02:15 -0800

    Fixed Parser1Test.

--- test/com/zhyfoundry/spider/impl/s1/Parser1Test.py
+++ test/com/zhyfoundry/spider/impl/s1/Parser1Test.py
@@ -11,46 +11,58 @@ class Parser1Test(unittest.TestCase):
         self.assertTrue(len(parseResult.newSeeds) == 2)
 
         parseResult2 = c.parse("""
-            <table class="bodycopy" border="0" cellspacing="0" cellpadding="2">
-                <tbody>
-                    <tr>
-                        <td width="50">&nbsp;</td>
-                        <td class="bold">Enterprise Name</td>
-                    </tr>
-                    <tr>
-                        <td width="50">&nbsp;</td>
-                        <td>aaa<br>bbb<br>ccc<br>ddd<br></td>
-                    </tr>
-                    <tr>
-                        <td width="50">&nbsp;</td>
-                        <td>Company Address</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50">&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50">&nbsp;</td>
-                        <td>&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50">Tel:</td>
-                        <td>123456</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50">Fax:</td>
-                        <td>234567</td>
-                    </tr>
-                    <tr>
-                        <td align="right" height="40" width="50">Email:</td>
-                        <td><a href="mailto:test@zhyfoundry.com">test@zhyfoundry.com</a></td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50">Web:</td>
-                        <td><a href="http://www.zhyfoundry.com/" target="_blank">www.zhyfoundry.com</a></td>
-                    </tr>
-                </tbody>
-            </table>
+
+<TABLE CLASS=bodycopy>
+    <TBODY>
+        <TR>
+            <TD>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td width="50">&nbsp;</td>
+                            <td class="bold">Enterprise Name</td>
+                        </tr>
+                        <tr>
+                            <td width="50">&nbsp;</td>
+                            <td>aaa<br>bbb<br>ccc<br>ddd<br></td>
+                        </tr>
+                        <tr>
+                            <td width="50">&nbsp;</td>
+                            <td>Company Address</td>
+                        </tr>
+                        <tr>
+                            <td align="right" width="50">&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td align="right" width="50">&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td align="right" width="50">Tel:</td>
+                            <td>123456</td>
+                        </tr>
+                        <tr>
+                            <td align="right" width="50">Fax:</td>
+                            <td>234567</td>
+                        </tr>
+                        <tr>
+                            <td align="right" height="40" width="50">Email:</td>
+                            <td><a href="mailto:test@zhyfoundry.com">test@zhyfoundry.com</a></td>
+                        </tr>
+                        <tr>
+                            <td align="right" width="50">Web:</td>
+                            <td><a href="http://www.zhyfoundry.com/" target="_blank">www.zhyfoundry.com</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>
+            </TD>
+        </TR>
+    </TBODY>
+</TABLE>
+
+
         """)
         self.assertEqual('Enterprise Name', parseResult2.content.name)
         self.assertEqual('test@zhyfoundry.com', parseResult2.content.email)
